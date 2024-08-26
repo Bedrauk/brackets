4e573ef1848bcf4dba56684c0313130c6c961ef0
Bedrauk <111381113+Bedrauk@users.noreply.github.com>
2024-08-26 22:58:35 +0300

    docs: finish?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.export = function check(str, bracketsConfig) {
+  const bracketsMap = Object.fromEntries(bracketsConfig);
+  
+
+  function isBalanced(s, index = 0, stack = []) {
+    if (index === s.length) {
+      return stack.length === 0;
+    }
+
+    const char = s[index];
+
+
+    if (char in bracketsMap) {
+      if (bracketsMap[char] === char) {
+        if (stack[stack.length - 1] === char) {
+          stack.pop()
+        } else {
+          stack.push(char)
+        }
+      } else {
+        stack.push(char)
+      }
+    } else {
+
+      const openingBracket = Object.keys(bracketsMap).find(key => bracketsMap[key] === char);
+      if (stack.length === 0 || stack[stack.length - 1] !== openingBracket) {
+        return false
+      }
+      stack.pop()
+    }
+
+
+    return isBalanced(s, index + 1, stack);
+  }
+
+
+  return isBalanced(str);
 }
